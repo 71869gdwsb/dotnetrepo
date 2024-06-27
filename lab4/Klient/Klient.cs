bea498c99e5072d0e952daf3dd75feb75c66d57c
71869gdwsb <gdx162342@student.gdansk.merito.pl>
2024-06-27 19:14:01 +0200

    projekt lab4

---
+++ lab4/Klient/Klient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+class Klient
+{
+    // klucz i wektor inicjalizujacy do aes
+    private static readonly byte[] klucz = Encoding.UTF8.GetBytes("sekretnyklucz123");
+    private static readonly byte[] iv = Encoding.UTF8.GetBytes("sekretnywektor12");
+
+    static void Main(string[] args)
+    {
+        // polaczenie z serwerem, port 8888
+        TcpClient klient = new TcpClient("127.0.0.1", 8888);
+        NetworkStream strumien = klient.GetStream();
+
+        // watek do odbierania wiadomosci
+        Thread odbieranieWatek = new Thread(OdbierajWiadomosci);
+        odbieranieWatek.Start(strumien);
+
+        Console.WriteLine("Połączono z serwerem. Wpisz wiadomości do wysłania:");
+
+        // petla do wysylania wiadomosci do serwera
+        while (true)
+        {
+            string wiadomosc = Console.ReadLine();
+            byte[] zaszyfrowanaWiadomosc = ZaszyfrujWiadomosc(wiadomosc);
+            strumien.Write(zaszyfrowanaWiadomosc, 0, zaszyfrowanaWiadomosc.Length);
+        }
+    }
+
+    // odbieranie wiadomosci
+    private static void OdbierajWiadomosci(object obj)
+    {
+        NetworkStream strumien = (NetworkStream)obj;
+        byte[] bufor = new byte[1024];
+        int odczytaneBajty;
+
+        // petla do odbierania i odszyfrowywania wiadomości
+        while ((odczytaneBajty = strumien.Read(bufor, 0, bufor.Length)) != 0)
+        {
+            string wiadomosc = OdszyfrujWiadomosc(bufor, odczytaneBajty);
+            Console.WriteLine("Odebrano: " + wiadomosc);
+        }
+    }
+
+    // szyfrowanie AES
+    private static byte[] ZaszyfrujWiadomosc(string wiadomosc)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = klucz;
+            aes.IV = iv;
+            ICryptoTransform szyfrator = aes.CreateEncryptor(aes.Key, aes.IV);
+            byte[] wiadomoscBajty = Encoding.UTF8.GetBytes(wiadomosc);
+            return szyfrator.TransformFinalBlock(wiadomoscBajty, 0, wiadomoscBajty.Length);
+        }
+    }
+
+    // odszyfrowanie AES
+    private static string OdszyfrujWiadomosc(byte[] bufor, int odczytaneBajty)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = klucz;
+            aes.IV = iv;
+            ICryptoTransform deszyfrator = aes.CreateDecryptor(aes.Key, aes.IV);
+            byte[] odszyfrowaneBajty = deszyfrator.TransformFinalBlock(bufor, 0, odczytaneBajty);
+            return Encoding.UTF8.GetString(odszyfrowaneBajty);
+        }
+    }
+}
---
+++ lab4/Serwer/Serwer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+class Serwer
+{
+    // lista do przechowywania polaczonych klientow
+    private static List<TcpClient> klienci = new List<TcpClient>();
+    // klucz i wektor inicjalizujacy do szyfrowania AES
+    private static readonly byte[] klucz = Encoding.UTF8.GetBytes("sekretnyklucz123");
+    private static readonly byte[] iv = Encoding.UTF8.GetBytes("sekretnywektor12");
+
+    static void Main(string[] args)
+    {
+        // instancja tcplistener sluchajaca na porcie 8888
+        TcpListener serwer = new TcpListener(IPAddress.Any, 8888);
+        serwer.Start();
+        Console.WriteLine("Serwer uruchomiony...");
+
+        // petla nasluchujaca nowych polaczen
+        while (true)
+        {
+            TcpClient klient = serwer.AcceptTcpClient();
+            lock (klienci)
+            {
+                klienci.Add(klient);
+            }
+            Console.WriteLine("Klient połączony...");
+            // tworzenie nowego watku dla kazdego polaczonego klienta
+            Thread klientWatek = new Thread(ObslugaKlienta);
+            klientWatek.Start(klient);
+        }
+    }
+
+    // metoda obsługujaca komunikacje z klientem
+    private static void ObslugaKlienta(object obj)
+    {
+        TcpClient klient = (TcpClient)obj;
+        NetworkStream strumien = klient.GetStream();
+        byte[] bufor = new byte[1024];
+        int odczytaneBajty;
+
+        // petla odbierajaca wiadomosci od klienta
+        while ((odczytaneBajty = strumien.Read(bufor, 0, bufor.Length)) != 0)
+        {
+            string wiadomosc = OdszyfrujWiadomosc(bufor, odczytaneBajty);
+            Console.WriteLine("Odebrano: " + wiadomosc);
+            RozeslijWiadomosc(wiadomosc);
+        }
+
+        // usuniecie klienta po zakonczeniu polaczenia
+        lock (klienci)
+        {
+            klienci.Remove(klient);
+        }
+        klient.Close();
+    }
+
+    // wysylanie do wszystkich podlaczonych klientow
+    private static void RozeslijWiadomosc(string wiadomosc)
+    {
+        byte[] zaszyfrowanaWiadomosc = ZaszyfrujWiadomosc(wiadomosc);
+        lock (klienci)
+        {
+            foreach (TcpClient klient in klienci)
+            {
+                NetworkStream strumien = klient.GetStream();
+                strumien.Write(zaszyfrowanaWiadomosc, 0, zaszyfrowanaWiadomosc.Length);
+            }
+        }
+    }
+
+    // szyfrowanie aes
+    private static byte[] ZaszyfrujWiadomosc(string wiadomosc)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = klucz;
+            aes.IV = iv;
+            ICryptoTransform szyfrator = aes.CreateEncryptor(aes.Key, aes.IV);
+            byte[] wiadomoscBajty = Encoding.UTF8.GetBytes(wiadomosc);
+            return szyfrator.TransformFinalBlock(wiadomoscBajty, 0, wiadomoscBajty.Length);
+        }
+    }
+
+    // odszyfrowanie aes
+    private static string OdszyfrujWiadomosc(byte[] bufor, int odczytaneBajty)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = klucz;
+            aes.IV = iv;
+            ICryptoTransform deszyfrator = aes.CreateDecryptor(aes.Key, aes.IV);
+            byte[] odszyfrowaneBajty = deszyfrator.TransformFinalBlock(bufor, 0, odczytaneBajty);
+            return Encoding.UTF8.GetString(odszyfrowaneBajty);
+        }
+    }
+}
